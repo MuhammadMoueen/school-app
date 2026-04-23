e0ac81a64e15a3c8fd917bc9cba817dcef6a55cb
Muhammad Moueen <s23bdocs1m01131@iub.edu.pk>
2026-04-23 21:18:56 +0500

    Add blank lines for better readability in urls.py

--- school_management/school_management/urls.py
+++ school_management/school_management/urls.py
@@ -25,6 +25,9 @@ urlpatterns = [
     path('', include('main.urls')),
 ]
 
+
+
+
 # Serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
